458ca7506eeea88fbab06ec9d273c71d95b76fc0
junlee91 <jlee757@uic.edu>
2018-12-04 22:57:09 -0600

    Apollo Codegen setting

---
+++ src/types/api.d.ts
@@ -0,0 +1,31 @@
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: startPhoneVerification
+// ====================================================
+
+export interface startPhoneVerification_StartPhoneVerfification {
+  __typename: "StartPhoneVerfificationResponse";
+  ok: boolean;
+  error: string | null;
+}
+
+export interface startPhoneVerification {
+  StartPhoneVerfification: startPhoneVerification_StartPhoneVerfification;
+}
+
+export interface startPhoneVerificationVariables {
+  phoneNumber: string;
+}
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
