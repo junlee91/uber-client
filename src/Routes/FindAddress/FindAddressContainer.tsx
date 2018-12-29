c1c78d8c58b43a0d8314f2e05741fb39916dd51e
junlee91 <jlee757@uic.edu>
2018-12-28 19:48:56 -0600

    Map Marker

--- src/Routes/FindAddress/FindAddressContainer.tsx
+++ src/Routes/FindAddress/FindAddressContainer.tsx
@@ -15,7 +15,15 @@ class FindAddressContainer extends React.Component<any> {
     const { google } = this.props;
     const maps = google.maps;
     const mapNode = ReactDOM.findDOMNode(this.mapRef.current);
-    this.map = new maps.Map(mapNode);
+    const mapConfig: google.maps.MapOptions = {
+      center: {
+        lat: 41.881534,
+        lng: -87.642030
+      },
+      disableDefaultUI: true,
+      zoom: 15,
+    }
+    this.map = new maps.Map(mapNode, mapConfig);
   }
 
   public render() {
