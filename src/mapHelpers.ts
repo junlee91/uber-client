3095dab16f5cc10dbcdc9aa85584bf37afd88924
junlee91 <jlee757@uic.edu>
2018-12-30 14:59:09 -0600

    Google maps directions and route

--- src/mapHelpers.ts
+++ src/mapHelpers.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import { GEO_KEY } from "./keys";
+import { MAPS_KEY } from "./keys";
 
 export const geoCode = async (address: string) => {
-  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GEO_KEY}`;
+  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${MAPS_KEY}`;
   const { data } = await axios(URL);
 
   if (!data.error_message) {
@@ -25,7 +25,7 @@ export const geoCode = async (address: string) => {
 };
 
 export const reverseGeoCode = async (lat: number, lng: number) => {
-  const URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GEO_KEY}`;
+  const URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${MAPS_KEY}`;
   const { data } = await axios(URL);
 
   if (!data.error_message) {
