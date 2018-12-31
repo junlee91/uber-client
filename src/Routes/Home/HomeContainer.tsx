ddb471727c308666b04a30ee7024f7c770c45b61
junlee91 <jlee757@uic.edu>
2018-12-30 23:42:37 -0600

    Bugfix

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -68,33 +68,33 @@ class HomeContainer extends React.Component<IProps, IState> {
     const { isMenuOpen, toAddress, price } = this.state;
     return (
       <ProfileQuery query={USER_PROFILE}>
-        {({ data, loading }) => {
-          if (data && data.GetMyProfile) {
-            const { GetMyProfile: { user = null } = {} } = ({} = data);
-            if (user) {
-              return (
-                <NearbyQueries query={GET_NEARBY_DRIVERS} skip={user.isDriving} onCompleted={this.handleNearbyDrivers}>
-                  {() => (
-                    <HomePresenter
-                      loading={loading}
-                      isMenuOpen={isMenuOpen}
-                      toggleMenu={this.toggleMenu}
-                      mapRef={this.mapRef}
-                      toAddress={toAddress}
-                      onInputChange={this.onInputChange}
-                      price={price}
-                      data={data}
-                      onAddressSubmit={this.onAddressSubmit}
-                    />
-                  )}
-                </NearbyQueries>
-              );
+        {({ data, loading }) => (
+          <NearbyQueries
+            query={GET_NEARBY_DRIVERS}
+            skip={
+              (data &&
+                data.GetMyProfile &&
+                data.GetMyProfile.user &&
+                data.GetMyProfile.user.isDriving) ||
+              false
             }
-            return null;
-          } else {
-            return "Loading";
-          }
-        }}
+            onCompleted={this.handleNearbyDrivers}
+          >
+            {() => (
+              <HomePresenter
+                loading={loading}
+                isMenuOpen={isMenuOpen}
+                toggleMenu={this.toggleMenu}
+                mapRef={this.mapRef}
+                toAddress={toAddress}
+                onInputChange={this.onInputChange}
+                price={price}
+                data={data}
+                onAddressSubmit={this.onAddressSubmit}
+              />
+            )}
+          </NearbyQueries>
+        )}
       </ProfileQuery>
     );
   }
@@ -278,13 +278,15 @@ class HomeContainer extends React.Component<IProps, IState> {
 
   public handleNearbyDrivers = (data: {} | getDrivers) => {
     if ("GetNearbyDrivers" in data) {
-      const { GetNearbyDrivers: { drivers, ok} } = data;
+      const {
+        GetNearbyDrivers: { drivers, ok }
+      } = data;
 
       if (ok && drivers) {
         console.log(drivers);
       }
     }
-  }
+  };
 }
 
 export default graphql<any, reportMovement, reportMovementVariables>(
