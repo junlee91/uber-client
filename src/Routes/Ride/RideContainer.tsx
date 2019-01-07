c1540515f1cccdec77a4893153d866337ac5087e
junlee91 <jlee757@uic.edu>
2019-01-06 21:17:31 -0600

    Ride Status Subscription

--- src/Routes/Ride/RideContainer.tsx
+++ src/Routes/Ride/RideContainer.tsx
@@ -1,12 +1,21 @@
+import { SubscribeToMoreOptions } from "apollo-client";
 import React from "react";
-import { Query } from "react-apollo";
-import { RouteComponentProps } from "react-router";
-import { getRide, getRideVariables } from "src/types/api";
-
+import { Mutation, Query } from "react-apollo";
+import { RouteComponentProps } from "react-router-dom";
+import { USER_PROFILE } from "../../sharedQueries";
+import {
+  getRide,
+  getRideVariables,
+  updateRide,
+  updateRideVariables,
+  userProfile
+} from "../../types/api";
 import RidePresenter from "./RidePresenter";
-import { GET_RIDE } from "./RideQueries";
+import { GET_RIDE, RIDE_SUBSCRIPTION, UPDATE_RIDE_STATUS } from "./RideQueries";
 
 class RideQuery extends Query<getRide, getRideVariables> {}
+class ProfileQuery extends Query<userProfile> {}
+class RideUpdate extends Mutation<updateRide, updateRideVariables> {}
 
 interface IProps extends RouteComponentProps<any> {}
 
@@ -26,9 +35,30 @@ class RideContainer extends React.Component<IProps> {
     } = this.props;
 
     return (
-      <RideQuery query={GET_RIDE} variables={{ rideId }}>
-        {rideData => <RidePresenter data={rideData} />}
-      </RideQuery>
+      <ProfileQuery query={USER_PROFILE}>
+        {({ data: userData }) => (
+          <RideQuery query={GET_RIDE} variables={{ rideId }}>
+            {({ data, loading, subscribeToMore }) => {
+              const subscribeOptions: SubscribeToMoreOptions = {
+                document: RIDE_SUBSCRIPTION
+              };
+              subscribeToMore(subscribeOptions);
+              return (
+                <RideUpdate mutation={UPDATE_RIDE_STATUS}>
+                  {updateRideFn => (
+                    <RidePresenter
+                      userData={userData}
+                      loading={loading}
+                      data={data}
+                      updateRideFn={updateRideFn}
+                    />
+                  )}
+                </RideUpdate>
+              );
+            }}
+          </RideQuery>
+        )}
+      </ProfileQuery>
     );
   }
 }
